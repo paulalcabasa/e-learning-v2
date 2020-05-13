65a942e71abdf8582f4322e6b67671483dff845e
Paul Alcabasa <paulalcabasa@gmail.com>
2020-05-13 16:01:43 +0800

    fix saving category admins

--- app/Models/CategoryAdmin.php
+++ app/Models/CategoryAdmin.php
@@ -15,31 +15,39 @@ class CategoryAdmin extends Model
     public $timestamps = false;
     protected $table = "category_administrators";
 
-    public function getAllByCategory($category_id){
-        $sql = "SELECT CONCAT(pit.first_name,' ',pit.last_name) admin_name,
+    public function getAdministrators(){
+            $sql = "SELECT CONCAT(pit.first_name,' ',pit.last_name) admin_name,
                         utt.user_type,
-                        ct.category_name,
-                        ca.id category_admin_id,	
-                        pit.employee_id,
-                        ct.id category_id,
-                        CASE WHEN ca.id IS NOT NULL THEN 1 ELSE 0 END admin_flag
+                        pit.employee_id
                 FROM ipc_central.user_access_tab uct
                     LEFT JOIN ipc_central.personal_information_tab pit
                         ON pit.employee_id = uct.employee_id
                     LEFT JOIN ipc_central.user_type_tab utt
                         ON utt.id = uct.user_type_id
-                    LEFT JOIN e_learning.category_administrators ca
-                        ON ca.employee_id = pit.employee_id 
-                        AND ca.category_id = :category_id 
-                    LEFT JOIN e_learning.categories ct
-                        ON ct.id = ca.category_id
-                        
                 WHERE 1 = 1
                     AND system_id = 47
                     AND utt.user_type = 'Administrator'
-                ORDER BY pit.last_name, pit.first_name";
-        $query = DB::select($sql, ['category_id' => $category_id]);
+                ORDER BY pit.last_name, pit.first_name"; 
+        $query = DB::connection('ipc_central')->select($sql);
         return $query;
+    }
+
+    public function getByUser($employee_id, $category_id){
+        $sql = "SELECT ca.category_id,
+                        ca.employee_id,
+                        ca.id
+                FROM e_learning.category_administrators ca
+                    LEFT JOIN e_learning.categories ct
+                        ON ct.id = ca.category_id
+                WHERE 1 = 1
+                    AND ca.category_id = :category_id
+                    AND ca.employee_id = :employee_id";
+        $params = [
+            'category_id' => $category_id,
+            'employee_id' => $employee_id
+        ];
+        $query = DB::select($sql, $params);
+        return count($query) > 0 ? $query[0] : $query;
     }
 
     public function batchInsert($params){
